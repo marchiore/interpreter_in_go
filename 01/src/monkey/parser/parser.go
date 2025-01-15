c1d6d38ec120d11d47b6a231b9b94c62f61b3cf6
marchiore <marchiore.matheus@gmail.com>
2025-01-15 20:11:00 -0300

    feat: include peekError and tests

--- 01/src/monkey/parser/parser.go
+++ 01/src/monkey/parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"01/ast"
 	"01/lexer"
 	"01/token"
+	"fmt"
 )
 
 type Parser struct {
@@ -11,10 +12,15 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
@@ -87,6 +93,17 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
